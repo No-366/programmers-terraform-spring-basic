55cd89deb22528f06225d4596f4b191ed7c86dc3
woongcheol Na <32201345@dankook.ac.kr>
2025-11-27 15:14:22 +0900

    github action 세팅

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -1,5 +1,6 @@
 package com.back
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,7 +10,7 @@ class HomeController(
     private val s3Service: S3Service // 추가해둔 의존성 -> S3 빈등록 가능 -> 빈으로 주입
 ) {
 
-    @Value("${custom.secretWord}")
+    @Value("\${custom.secretWord}")
     private val secretWord: String = ""
 
     @GetMapping
