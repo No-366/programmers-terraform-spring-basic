ed4dbab0be0ceb9733f0a696fd7c4f8dc309cdd5
woongcheol Na <32201345@dankook.ac.kr>
2025-11-27 13:53:54 +0900

    github action 세팅

--- src/main/kotlin/com/back/HomeController.kt
+++ src/main/kotlin/com/back/HomeController.kt
@@ -2,13 +2,16 @@ package com.back
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import software.amazon.awssdk.services.s3.S3Client
 
 
 @RestController
 class HomeController(
-    private val s3Client: S3Client // 추가해둔 의존성 -> S3 빈등록 가능 -> 빈으로 주입
+    private val s3Service: S3Service // 추가해둔 의존성 -> S3 빈등록 가능 -> 빈으로 주입
 ) {
+
+    @Value("${custom.secretWord}")
+    private val secretWord: String = ""
+
     @GetMapping
     fun main():String{
         return "Hello World!"
@@ -16,6 +19,6 @@ class HomeController(
 
     @GetMapping("/buckets")
     fun buckens(): List<String>{
-        return s3Client.listBuckets().buckets().map{ it.name() }
+        return s3Service.getBucketNames()
     }
 }
